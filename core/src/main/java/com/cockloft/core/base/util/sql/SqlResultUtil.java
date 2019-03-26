3d62e67d8de56d33c2e348287cbbb0494a208ab6
kyssion <1409915687@qq.com>
2019-03-26 23:39:17 +0800

    update

--- core/src/main/java/com/cockloft/core/base/util/sql/SqlResultUtil.java
+++ core/src/main/java/com/cockloft/core/base/util/sql/SqlResultUtil.java
@@ -5,8 +5,4 @@ import io.vertx.ext.sql.ResultSet;
 import java.lang.reflect.InvocationTargetException;
 
 public class SqlResultUtil {
-
-    public static <T> T getResultUtil(ResultSet resultSet, Class<T> classItem) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        return (T) new Object();
-    }
 }
