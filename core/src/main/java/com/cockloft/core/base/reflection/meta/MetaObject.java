b6804c507a9ff73f1ac319d1aff815d2574726fb
kyssion <1409915687@qq.com>
2019-03-27 20:08:31 +0800

    update

--- core/src/main/java/com/cockloft/core/base/reflection/meta/MetaObject.java
+++ core/src/main/java/com/cockloft/core/base/reflection/meta/MetaObject.java
@@ -19,6 +19,7 @@ public class MetaObject {
     private final ObjectFactory objectFactory;
     private final ObjectWrapperFactory objectWrapperFactory;
     private final ReflectorFactory reflectorFactory;
+
     @SuppressWarnings("unchecked")
     private MetaObject(Object object, ObjectFactory objectFactory,
                        ObjectWrapperFactory objectWrapperFactory, ReflectorFactory reflectorFactory) {
@@ -32,7 +33,7 @@ public class MetaObject {
         } else if (objectWrapperFactory.hasWrapperFor(object)) {
             this.objectWrapper = objectWrapperFactory.getWrapperFor(this, object);
         } else if (object instanceof Map) {
-            this.objectWrapper = new MapWrapper(this, (Map<String,Object>) object);
+            this.objectWrapper = new MapWrapper(this, (Map<String, Object>) object);
         } else if (object instanceof Collection) {
             this.objectWrapper = new CollectionWrapper(this, (Collection) object);
         } else {
