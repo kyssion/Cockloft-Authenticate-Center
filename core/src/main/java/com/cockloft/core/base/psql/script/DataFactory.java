dc93bab9bc2b645b1dd5c8d9ad3f57c6ba5be0ac
kyssion <1409915687@qq.com>
2019-03-31 22:30:03 +0800

    update

--- core/src/main/java/com/cockloft/core/base/psql/script/DataFactory.java
+++ core/src/main/java/com/cockloft/core/base/psql/script/DataFactory.java
@@ -3,5 +3,6 @@ package com.cockloft.core.base.psql.script;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DataFactory {
-    private ConcurrentHashMap<String,DataItem> DataMap;
+    private ConcurrentHashMap<Class,DataItem> DataMap;
+
 }
