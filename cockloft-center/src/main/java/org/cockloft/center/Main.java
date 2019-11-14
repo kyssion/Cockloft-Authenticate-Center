c8bb79078d608620c96a28a562c522a06c62d171
kyssion <1409915687@qq.com>
2019-11-14 18:49:59 +0800

    init center

--- cockloft-center/src/main/java/org/cockloft/center/Main.java
+++ cockloft-center/src/main/java/org/cockloft/center/Main.java
@@ -2,9 +2,9 @@ package org.cockloft.center;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
-import io.vertx.core.WorkerExecutor;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
+import org.cockloft.vertx.router.Router;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,37 +14,12 @@ public class Main {
     public static void main(String[] args) {
         VertxOptions vertxOptions = new VertxOptions();
         Vertx vertx = Vertx.vertx(vertxOptions);
-        HttpServerOptions httpServerOptions= new HttpServerOptions();
-        HttpServer httpServer = vertx.createHttpServer(httpServerOptions);
-        WorkerExecutor workerExecutor = vertx.createSharedWorkerExecutor("sdfsdf");
-        workerExecutor.executeBlocking((proms)->{
+        Router router = Router.router(vertx);
 
-        },res->{
+        HttpServerOptions httpServerOptions = new HttpServerOptions();
+        HttpServer server = vertx.createHttpServer(httpServerOptions);
+        server.requestHandler(router).listen(8080,(res)->{
 
-        });
-        httpServer.requestHandler((res)->{
-            logger.info("{}",res.path());
-            res.response().end("hello world");
-
-        });
-
-        workerExecutor.executeBlocking(
-                (ttt)->{
-
-                },(res)->{
-
-                }
-        );
-        httpServer.listen(8080,(vo)->{
-           if(vo.succeeded()){
-               logger.info("cockloft starting succuess ! port : {}",8080);
-           }else{
-               if(vo.cause()!=null) {
-                   logger.error("cockloft starting failed , error msg : {}", vo.cause().getCause().getMessage());
-               }else{
-                   logger.error("cockloft starting failed , unknow error");
-               }
-           }
         });
     }
 }
