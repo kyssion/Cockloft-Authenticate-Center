3534ef10893b731aab8aabbb26bbb36e8bcae774
kyssion <1409915687@qq.com>
2019-12-02 21:04:26 +0800

    feat : add authentication handle

---
+++ user-authentication/src/main/java/org/cockloft/org/cockloft/user/authentication/handle/AuthenticationHandle.java
@@ -0,0 +1,24 @@
+package org.cockloft.org.cockloft.user.authentication.handle;
+
+import io.vertx.core.MultiMap;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import org.cockloft.vertx.router.RouteContext;
+import org.cockloft.vertx.router.handlers.RouterHandler;
+
+/**
+ * autheen
+ */
+public class AuthenticationHandle implements RouterHandler<RouteContext> {
+    @Override
+    public void handle(RouteContext routeContext) {
+        HttpServerRequest request = routeContext.getRequest();
+        HttpServerResponse response = routeContext.getResponse();
+        MultiMap urlParams = routeContext.getParams();
+        String redirect = urlParams.get("redirect_url");
+        String statusCode = urlParams.get("status_code");
+        response.putHeader("redirect_url",redirect);
+        response.putHeader("status_code",statusCode);
+
+    }
+}
